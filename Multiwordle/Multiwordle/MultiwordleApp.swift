e2ae6f690f34f983ebd5d9baea1ecb84a1991802
EJ Gaygay <ejgaygay@EJs-MacBook-Air.fios-router.home>
2024-10-27 00:10:32 -0400

    init project

---
+++ Multiwordle/Multiwordle/MultiwordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiwordleApp.swift
+//  Multiwordle
+//
+//  Created by EJ Gaygay on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MultiwordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
